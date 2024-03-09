83089b4cd4a1489273616ec6402ef66bdb19eba3
Justin Shaber <justinshaber@gmail.com>
2024-03-09 14:12:28 -0700

    add search function and route that checks if any chapters contain certain text. Renders linked list of results

--- book_viewer.rb
+++ book_viewer.rb
@@ -12,6 +12,30 @@ helpers do
       "<p>#{paragraph}</p>"
     end.join
   end
+
+  def each_chapter
+    @contents.each_with_index do |title, num|
+      num += 1
+      chapter = File.read "data/chp#{num}.txt"
+      yield title, num, chapter
+    end
+  end
+
+  def search_chapters(query)
+    results = []
+
+    return results if !query || query.empty?
+
+    each_chapter do |title, num, content|
+      results << { :title => title, :number => num } if content.include? query
+    end
+
+    results
+  end
+end
+
+not_found do
+  redirect "/"
 end
 
 get "/" do
@@ -23,9 +47,18 @@ end
 get "/chapters/:number" do
   number = params[:number].to_i
   chapter_title = @contents[number - 1]
-  @title = "Chapter #{number}: #{chapter_title}"
 
+  redirect "/" unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_title}"
   @chapter = File.read "data/chp#{number}.txt"
 
   erb :chapter
+end
+
+get "/search" do
+  @title = "Search"
+  @results = search_chapters(params[:query])
+
+  erb :search
 end
