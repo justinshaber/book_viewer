319d6e79d0c1f209ec42070826bb09f358a806f8
Justin Shaber <justinshaber@gmail.com>
2024-03-09 15:59:15 -0700

    updated the search to link to specific paragraphs of the text that match the search phrase.

--- book_viewer.rb
+++ book_viewer.rb
@@ -8,8 +8,8 @@ end
 
 helpers do
   def in_paragraphs(text)
-    text.split("\n\n").map do |paragraph|
-      "<p>#{paragraph}</p>"
+    text.split("\n\n").map.with_index do |paragraph, idx|
+      "<p id = #{idx + 1}>#{paragraph}</p>"
     end.join
   end
 
@@ -27,7 +27,13 @@ helpers do
     return results if !query || query.empty?
 
     each_chapter do |title, num, content|
-      results << { :title => title, :number => num } if content.include? query
+      paragraphs = []
+
+      content.split("\n\n").each_with_index do |paragraph, idx|
+        paragraphs << { :num => idx + 1, :content => paragraph } if paragraph.include? query
+      end
+      
+      results << { :title => title, :number => num, :paragraphs => paragraphs } if paragraphs.any?
     end
 
     results
@@ -45,6 +51,18 @@ get "/" do
 end
 
 get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_title = @contents[number - 1]
+
+  redirect "/" unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_title}"
+  @chapter = File.read "data/chp#{number}.txt"
+
+  erb :chapter
+end
+
+get "/chapters/:number/" do
   number = params[:number].to_i
   chapter_title = @contents[number - 1]
 
