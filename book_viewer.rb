db702b1710c2bec0ae4b1bb1921b2f7a9905ba32
Justin Shaber <justinshaber@gmail.com>
2024-03-08 17:11:51 -0700

    Updated the links in views/layout.erb and views/home.erb to link to the new book pages.

--- book_viewer.rb
+++ book_viewer.rb
@@ -8,10 +8,13 @@ get "/" do
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
+get "/chapters/:number" do
   @contents = File.readlines "data/toc.txt"
-  @chapter = File.read "data/chp1.txt"
+
+  number = params[:number]
+  @title = "Chapter #{number}"
+
+  @chapter = File.read "data/chp#{number}.txt"
 
   erb :chapter
 end
